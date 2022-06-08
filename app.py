5ce982956abdbd2d787dbd2265a4954c88eaa922
abridevops <105608743+abridevops@users.noreply.github.com>
2022-06-08 03:12:08 -0400

    car servicing feature

--- app.py
+++ app.py
@@ -4,4 +4,5 @@ print('silvia')
 print('Landmark software solutions')
 print('buamess solutions consultancy')
 #bugs removed
-
+print('Car service automated')
+print('Hello $user, your car is due for servicing')
